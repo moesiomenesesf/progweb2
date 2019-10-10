c5970403122ba5e72aee668bd53220bf7b9d30db
Moésio <moesio.meneses@ext.chemtech.net.br>
2019-10-10 09:13:23 -0300

    trab2 concluido

--- Trabalho 02/Backend/index.js
+++ Trabalho 02/Backend/index.js
@@ -1,17 +1,22 @@
 
 const express = require('express');
 const app = express();         
+const { IncomingForm } = require('formidable');
+
+
 const bodyParser = require('body-parser');
 const port = 3000; //porta padrão
 const mysql = require('mysql');
 const testFolder = '../Arquivos/';
 const fs = require('fs');
-const http = require('http');
-var url = require('url');
-var exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
 var session = require('express-session');
 var path = require('path');
+const cors = require('cors');
+const mime = require('mime');
+const mv = require('mv');
+var rimraf = require("rimraf");
+app.use(cors());
+
 
 const connection = mysql.createConnection({
   host     : 'localhost',
@@ -79,14 +84,108 @@ app.get('/arquivos', function(request, response) {
 	response.end();
 });
 
-app.get('/baixar', function(req, response){
-	var folder = "";
-	const file = fs.createWriteStream("file.txt");
-	if(req.query.folder){
-		folder = req.query.folder;
+
+
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname + '/web/index.html'));
+
+});
+
+app.get('/download', (req, res) => {
+
+	filePath2 = req.query.folder;
+    let filePath = path.join(__dirname + '/'+ `${testFolder+request.session.username+filePath2}`);
+
+    res.download(filePath, fileName, (err) => { 
+
+        if (err) throw err;
+
+        console.log('File downloaded');
+
+    });
+
+});
+
+app.post('/createAccount', (req, res) =>{
+    var username = request.body.username;
+	var password = request.body.password;
+    execSQLQuery(`INSERT INTO accounts (username, password) VALUES('${username}' ,'${password}')`, res, connection);
+});
+
+app.get('/createFolder', (req,res) => {
+	let filePath = req.query.folder;
+	let dir = testFolder+request.session.username+filePath+'/'+req.query.folderName;
+	if (!fs.existsSync(dir)){
+		fs.mkdirSync(dir);
+	}else{
+		console.log("não da pra criar");
 	}
-	
-	response.pipe(`C:/Users/e19ju/OneDrive/Desktop/Programação Web II/Trabalho 01/Frontend/index.html`);
+})
+
+app.get('/deleteFile', (req, res) => {
+	let filePath = req.query.folder;
+	let dir = testFolder+request.session.username+filePath;
+	fs.unlink(dir, function (err) {
+		if (err) throw err;
+		console.log('deletou');
+	  });
+})
+
+app.get('/deleteFolder', (req,res) => {
+	let filePath = req.query.folder;
+	let dir = testFolder+request.session.username+filePath+'/'+req.query.folderName;
+	rimraf.sync(dir);
+});
+
+app.post('/upload', (req, res) => {
+
+    let form = new IncomingForm();
+
+    let user = req.session.username;
+
+    let userDir = path.join(__dirname + `/Arquivos/${user}/`);
+
+    //IMPORTANTE, tem que verificar se o diretório que tu vai salvar o arquivo existe, senão existir ele dá erro na hora de mover
+    if(!fs.existsSync(userDir)){
+
+        fs.mkdirSync(userDir);
+
+    }
+
+    form.parse(req, (err, fields, files) => {
+
+        let fileName = files.filetoupload.name;
+        let fileMimeType = mime.getType()
+
+        
+
+        let oldpath = files.filetoupload.path;
+        let newpath = path.join(__dirname + `/Arquivos/${user}/` + fileName);
+        
+        mv(oldpath, newpath, function (err) {
+            
+            if (err) throw err;
+            
+            console.log(`File uploaded to ${newpath}`);
+            let fileName = files.filetoupload.name;
+            let fileMimeType = mime.getType(newpath);
+            let fileSize = fs.statSync(newpath).size;
+
+            //Metadados úteis do arquivo que podem ser salvos no BD para fácil acesso
+            console.log(`File Path: ${newpath}`); //Sempre salvar esse carinha no banco para recuperar o arquivo no disco. Nunca salve o blob direto no banco, senão vc merece uma pea
+            console.log(`File Name: ${fileName}`);    
+            console.log(`File Type: ${fileMimeType}`);
+            console.log(`File Size: ${fileSize} bytes`); //Útil para controle de utilização de armazenamento
+
+            res.send('Arquivo subido zé!');
+
+            
+        });
+
+    });
+
 });
 
 app.get('/logout', function(req, res){
@@ -105,3 +204,15 @@ app.get('/logout', function(req, res){
 
 
 app.listen(port);
+
+function execSQLQuery(sqlQry, res, connection){
+    
+    connection.query(sqlQry, function(error, results, fields){
+        if(error) 
+          res.json(error);
+        else
+          res.json(results);
+        connection.end();
+        console.log('executou!');
+    });
+  }
